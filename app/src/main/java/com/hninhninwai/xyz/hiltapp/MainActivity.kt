37fa82bc36a0724cf1fd3d8c6f4764b2416889f5
shwehnin <hninhnin6398@gmail.com>
2025-11-10 17:16:46 +0700

    Android Entry Point : inject repository

--- app/src/main/java/com/hninhninwai/xyz/hiltapp/MainActivity.kt
+++ app/src/main/java/com/hninhninwai/xyz/hiltapp/MainActivity.kt
@@ -6,8 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+// AndroidEntryPoint
+// Mark android components (Activities, Fragments,..) as injection targets
+// It allows Hilt to provide dependencies directly into these components
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // Inject Repository
+    @Inject
+    lateinit var greetingRepository: GreetingRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +31,6 @@ class MainActivity : AppCompatActivity() {
 
         // Find the text view
         val greetingText : TextView = findViewById(R.id.greetingText)
+        greetingText.text = greetingRepository.sayHello()
     }
 }
